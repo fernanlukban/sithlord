f864f5aeb9488b5dc3413b9cc3451f21dc45b63a
Fernan Lukban <fernanlukban@gmail.com>
2018-02-18 11:44:08 -0800

    Fixed main.py to show the absolute words and fixed the json file for the words

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from json import load as json_load
 
 app = Flask(__name__)
 
+#Absolute words stuff
+absolute_words_file_name = 'absWords.json'
+absolute_words_file = open(absolute_words_file_name)
+absolute_words_json = json_load(absolute_words_file)
+absolute_words = set(absolute_words_json['data'])
+
 @app.route("/")
 def root():
-    return "Hello World!"
+    return ", ".join(absolute_words)
 
 if __name__ == '__main__':
     app.run()
